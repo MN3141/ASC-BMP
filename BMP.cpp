be1609be1295ad0534e0cbfed071ef95d036ba2d
NEAG MIHAI <mihaineag6@gmail.com>
2023-12-09 23:43:35 +0200

    Nevoie de implementarea anteturilor pentru fisierele *.bmp

--- BMP.cpp
+++ BMP.cpp
@@ -1,6 +1,6 @@
 //---------------------------------------------------------------------------
-
-
+/*fisierele *.bmp stocheaza pixelii de jos in sus,de la stanga la dreapta
+  fisierele *.bmp folosesc notatia little endian*/
 #pragma hdrstop
 
 #include "BMP.h"
@@ -13,6 +13,10 @@ void BMP:: createImage() //OBS:EM(end of medium) are codul ASCII x19
 {
        this->imageFile->WriteBuffer(this->pixels+1,this->numPixels*3);  //OBS:WriteBuffer ia buffer-ul de la indicele 0
                                                                       //dar string-ul pleaca de la indicele 1!!!                                                  
+}
+void BMP::createText()
+{
+
 }
 BMP::~BMP()//necesar dezalocarii memoriei
 {
